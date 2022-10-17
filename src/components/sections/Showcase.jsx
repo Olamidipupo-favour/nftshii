1510e0e071ed37007995280cb66c8b4cb4575cdb
Agboola Olamidipupo Favour <olamidipupofavour@codecube.com.ng>
2022-10-18 00:09:57 +0100

    fixed uo team mates

--- src/components/sections/Showcase.jsx
+++ src/components/sections/Showcase.jsx
@@ -2,16 +2,6 @@ import React, { useRef } from 'react'
 import styled, { keyframes } from 'styled-components'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import img1 from '../../assets/Nfts/bighead.svg';
-import img2 from '../../assets/Nfts/bighead-1.svg';
-import img3 from '../../assets/Nfts/bighead-2.svg';
-import img4 from '../../assets/Nfts/bighead-3.svg';
-import img5 from '../../assets/Nfts/bighead-4.svg';
-import img6 from '../../assets/Nfts/bighead-5.svg';
-import img7 from '../../assets/Nfts/bighead-6.svg';
-import img8 from '../../assets/Nfts/bighead-7.svg';
-import img9 from '../../assets/Nfts/bighead-8.svg';
-import img10 from '../../assets/Nfts/bighead-9.svg';
 import ETH from '../../assets/icons8-ethereum-48.png'
 const Toast = (story) => {
         toast.info(story, {
